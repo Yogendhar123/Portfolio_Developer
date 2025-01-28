fed2f7d0de9fe1286e5d4b1b40d03b2194fdcc15
Yogendhar123 <Yogendhar@1>
2025-01-28 15:15:32 +0530

    test_case_direct

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Yogendhar</span>
+            Hi, I'm <span className="text-[#915EFF]">Yogendhar Sri Ram</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop responsive UIs and web applications using React js and
